3413af5675d888d1ddf47dad01cc96be0e4ae893
KspLite <ksplite@outlook.com>
2023-06-06 21:33:04 +0800

    decorate BottomBar

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <div id='BottomBar' v-if="Page == 'User'">
-            <button v-on:click="ChangeCheck">查询</button>
-            <button v-on:click="ChangeMap">地图</button>
+    <div class="BottomBarContainer">
+        <div id='BottomBar' class="BottomBar" v-if="Page == 'User'">
+            <button v-on:click="ChangeCheck">
+                <img class="BottomBarSvg" src="../assets/home.svg">
+                <div>查询</div>
+            </button>
+            <button v-on:click="ChangeMap">
+                <img class="BottomBarSvg" src="../assets/map.svg">
+                <div>地图</div>
+            </button>
         </div>
-        <div id='BottomBar' v-else-if="Page == 'Manager' && ManagerFun == 'CheckDelete'">
-            <button v-on:click="ChangeAddEdit">新增</button>
-            <button v-on:click="DeleteClassRoom">删除</button>
+        <div id='BottomBar' class="BottomBar" v-else-if="Page == 'Manager' && ManagerFun == 'CheckDelete'">
+            <button v-on:click="ChangeAddEdit">
+                <img class="BottomBarSvg" src="../assets/add_circle.svg">
+                <div>新增</div>
+            </button>
+            <button v-on:click="DeleteClassRoom">
+                <img class="BottomBarSvg" src="../assets/delete.svg">
+                <div>删除</div>
+            </button>
         </div>
-        <div id='BottomBar' v-else-if="Page == 'Manager' && ManagerFun == 'RecycleBin'">
-            <button v-on:click="RecoverClassRoom">恢复</button>
+        <div id='BottomBar' class="BottomBar" v-else-if="Page == 'Manager' && ManagerFun == 'RecycleBin'">
+            <button v-on:click="RecoverClassRoom">
+                <img class="BottomBarSvg" src="../assets/undo.svg">
+                <div>恢复</div>
+            </button>
         </div>
     </div>
 </template>
@@ -40,70 +55,30 @@
     }
 </script>
 
-
-<!--
-    <template>
-    <div :class="bottomContainer" v-if="hidden != true">
-        <div v-if="pageStatus === 'user'">
-            <button name="query" @click="clickQuery"><el-icon><House /></el-icon>查询</button>
-            <button name="map" @click="clickMap"><el-icon><Compass /></el-icon>地图</button>
-        </div>
-        <div v-else-if="pageStatus === 'admin'">
-            <button name="add" @click="clickAdd"><el-icon><CirclePlus /></el-icon>新增</button>
-            <button name="remove" @click="clickRemove"><el-icon><Delete /></el-icon>删除</button>
-        </div>
-        <div v-else-if="pageStatus === 'trash'">
-            <button name="restore" @click="clickRestore"><el-icon><RefreshLeft /></el-icon>恢复</button>
-            <button name="delete" @click="clickDelete"><el-icon><Delete /></el-icon>永久删除</button>
-        </div>
-    </div>
-</template>
-
-<script>
-    export default {
-        data() {
-            return {
-                hidden:false
-            }
-        },
-        props: {
-            pageStatus:{
-                default:'user'
-            }  //可为'user', 'admin', 'trash', 'hidden' 
-        },
-        emits: ['../pages/UserCheck', '../pages/AddEdit', '../pages/UserMap'],
-        methods: {
-            clickQuery() {
-                this.$emit('../pages/UserCheck', 'query')//切换至查询页
-            },
-            clickMap() {
-                this.$emit('../pages/UserMap', 'map')//切换至地图页
-            },
-            clickAdd() {
-                this.hidden = true
-                this.$emit('../pages/AddEdit', true)//打开新增页
-            },
-            clickRemove() {
-                //选中逻辑位于管理员主页，待补充
-            },
-            clickRestore() {
-                //选中逻辑位于回收站页，待补充
-            },
-            clickDelete() {
-                //选中逻辑位于回收站页，待补充
-            }
-        }
-    }
-</script>
-
 <style>
-    .bottomContainer {
-        align-self: center;
+    .BottomBarContainer {
         position: fixed;
-        bottom: 20px;
+        bottom: 0;
+        width: 100%;
+        z-index: 100;
+    }
+    .BottomBar {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
     }
     button {
-        color: aqua;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        border: 1px solid grey;
+        width: 100%;
+        float: left;
+        background-color: white;
+    }
+    .BottomBarSvg {
+        height: 36px;
     }
 </style>
--->
